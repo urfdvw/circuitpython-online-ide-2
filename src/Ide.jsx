4048978cccab36028375938fc1f52e2561f4f552
术生何 <urfdvw@gmail.com>
2023-09-04 16:56:28 -0400

    small bug fix

--- src/Ide.jsx
+++ src/Ide.jsx
@@ -5,9 +5,6 @@ import * as FlexLayout from "flexlayout-react";
 import layout from "./layout.json";
 // ace
 import AceEditor from "react-ace";
-import "ace-builds/src-noconflict/mode-markdown";
-import "ace-builds/src-noconflict/theme-cloud9_day";
-import "ace-builds/src-noconflict/ext-language_tools";
 
 export default function Ide() {
     const [model, setModel] = useState(FlexLayout.Model.fromJson(layout));
@@ -18,18 +15,7 @@ export default function Ide() {
         if (component === "editor") {
             return (
                 <div className="tab_content">
-                    <AceEditor
-                        mode="markdown"
-                        theme="cloud9_day"
-                        name="UNIQUE_ID_OF_DIV"
-                        editorProps={{ $blockScrolling: true }}
-                        width="100%"
-                        height="100%"
-                        value={text}
-                        onChange={(newValue) => {
-                            setText(newValue);
-                        }}
-                    />
+                    <AceEditor />
                 </div>
             );
         } else if (component === "preview") {

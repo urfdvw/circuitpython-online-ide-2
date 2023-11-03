76878283599e97bd12250316c5f47aa8b2cfc6fc
术生何 <urfdvw@gmail.com>
2023-11-03 15:07:19 -0400

    refine popup logic

--- src/PopUp.jsx
+++ src/PopUp.jsx
@@ -1,17 +1,26 @@
 import { useState } from "react";
 import NewWindow from "react-new-window";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import Button from "@mui/material/Button";
 
 export default function PopUp({ children, title }) {
     const [popped, setPopped] = useState(false);
     return popped ? (
         <>
-            <button
+            <p>This tab is opened in a popup window.</p>
+            <Button
                 onClick={() => {
                     setPopped(false);
                 }}
+                style={{
+                    textTransform: "none",
+                }}
+                variant="contained"
             >
-                dock the window
-            </button>
+                Dock the window
+            </Button>
             <NewWindow
                 title={title}
                 onUnload={() => {
@@ -23,13 +32,15 @@ export default function PopUp({ children, title }) {
         </>
     ) : (
         <>
-            <button
-                onClick={() => {
-                    setPopped(true);
-                }}
-            >
-                pop the window
-            </button>
+            <Tooltip title="Open the tab in a popup window" sx={{ position: "absolute", top: 3, right: 3, zIndex:1}}>
+                <IconButton>
+                    <OpenInNewIcon
+                        onClick={() => {
+                            setPopped(true);
+                        }}
+                    />
+                </IconButton>
+            </Tooltip>
             {children}
         </>
     );

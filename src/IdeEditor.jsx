8ca09d25022b13060644bd02eadd542c22e4e485
术生何 <urfdvw@gmail.com>
2023-11-03 14:41:05 -0400

    test another popup lib, working

--- src/IdeEditor.jsx
+++ src/IdeEditor.jsx
@@ -1,17 +1,51 @@
 /* eslint-disable react/prop-types */
 
 // ace
+import { useState } from "react";
 import AceEditor from "react-ace";
+import NewWindow from "react-new-window";
 
 export default function IdeEditor({ fileKey, fileLookUp, setFileLookUp }) {
-    return (
-        <AceEditor
-            value={fileLookUp[fileKey].userText}
-            // onChange={(newValue) => {
-            //     setFileLookUp((cur) => {
-            //         return { ...cur, [fileKey]: { ...cur[fileKey], userText: newValue } };
-            //     });
-            // }} // this will make the editor very slow
-        />
+    const [popped, setPopped] = useState(false);
+    const [text, setText] = useState("");
+    return popped ? (
+        <>
+            <button
+                onClick={() => {
+                    setPopped(false);
+                }}
+            >
+                dock the window
+            </button>
+            <NewWindow
+                title={fileLookUp[fileKey].handle.name}
+                onUnload={() => {
+                    setPopped(false);
+                }}
+            >
+                <AceEditor
+                    value={text}
+                    onChange={(newValue) => {
+                        setText(newValue);
+                    }} // this will make the editor very slow
+                />
+            </NewWindow>
+        </>
+    ) : (
+        <>
+            <button
+                onClick={() => {
+                    setPopped(true);
+                }}
+            >
+                pop the window
+            </button>
+            <AceEditor
+                value={text}
+                onChange={(newValue) => {
+                    setText(newValue);
+                }} // this will make the editor very slow
+            />
+        </>
     );
 }

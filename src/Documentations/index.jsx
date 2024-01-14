1fe9bded0bd932442848411160ba9c3bcd44582a
River Wang <urfdvw@gmail.com>
2024-01-13 23:18:33 -0500

    remove sub module and remove md documentation

--- src/Documentations/index.jsx
+++
@@ -1,7 +0,0 @@
-import Markdown from "markdown-to-jsx";
-import MdFile from "../CircuitPython-online-IDE2.wiki/About.md?raw";
-// import MdFile from "./test.md?raw" 
-
-export function About() {
-    return <Markdown>{MdFile}</Markdown>;
-}

6ad22091ada23fb9f6cd038105e56f9f986335ad
River Wang <urfdvw@gmail.com>
2024-03-25 12:02:18 -0400

    move about to generic Document component

--- src/tabs/Document.jsx
+++ src/tabs/Document.jsx
@@ -1,9 +1,8 @@
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import MarkdownExtended from "../layout/MarkdownExtended";
-import info from "../documents/wiki/About.md";
 
-export default function About() {
+export default function Document({ info }) {
     return (
         <Box>
             <Typography id="modal-modal-description" sx={{ mt: 2, margin: "20pt" }}>

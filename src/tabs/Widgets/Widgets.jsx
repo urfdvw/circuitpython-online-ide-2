577b56c7d3c85c9255d6964a99a91840bccaa515
River Wang <urfdvw@gmail.com>
2024-05-23 23:47:24 -0400

    [bug] changes in Widgets only

--- src/tabs/Widgets/Widgets.jsx
+++ src/tabs/Widgets/Widgets.jsx
@@ -1,3 +1,10 @@
+import useConnectedVariables from "./useConnectedVariables";
+import { useContext } from "react";
+import ideContext from "../../ideContext";
+
 export default function Widgets() {
+    // const { serialOutput } = useContext(ideContext);
+    // const { setVariable, getVariable, connectedVariables } = useConnectedVariables(serialOutput);
+    // console.log(connectedVariables);
     return <>Widgets Tab</>;
 }

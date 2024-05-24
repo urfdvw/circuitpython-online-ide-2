39e7b236b294dae12768a6bea3c3bf73d3adcd38
River Wang <urfdvw@gmail.com>
2024-05-24 00:05:21 -0400

    received  cv as object

--- src/tabs/Widgets/Widgets.jsx
+++ src/tabs/Widgets/Widgets.jsx
@@ -1,10 +1,12 @@
 import useConnectedVariables from "./useConnectedVariables";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import ideContext from "../../ideContext";
 
 export default function Widgets() {
-    // const { serialOutput } = useContext(ideContext);
-    // const { setVariable, getVariable, connectedVariables } = useConnectedVariables(serialOutput);
-    // console.log(connectedVariables);
+    const { serialOutput } = useContext(ideContext);
+    const { setVariable, getVariable, connectedVariables } = useConnectedVariables(serialOutput);
+    useEffect(() => {
+        console.log(connectedVariables);
+    }, [connectedVariables]);
     return <>Widgets Tab</>;
 }

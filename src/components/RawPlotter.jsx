60d228d8e7e109729651056bb6c28c30879199dd
River Wang <urfdvw@gmail.com>
2025-07-12 11:31:21 -0400

    skip compute when no data in plot

--- src/components/RawPlotter.jsx
+++ src/components/RawPlotter.jsx
@@ -49,73 +49,74 @@ export default function RawPlotter({ node }) {
     var showlegend = config.plot.show_legend;
 
     const output = removeInBetween(serialOutput, constants.CV_JSON_START, constants.CV_JSON_END);
+    if (output || output.trim() !== "") {
+        try {
+            const last_active_block = output.split("soft reboot").at(-1);
+            // console.log("RawPlotter", "Last active block:", last_active_block);
+            const last_plot_text = last_active_block.split("startplot:").at(-1);
+            console.log("RawPlotter", "Last plot data:", last_plot_text);
 
-    try {
-        const last_active_block = output.split("soft reboot").at(-1);
-        // console.log("RawPlotter", "Last active block:", last_active_block);
-        const last_plot_text = last_active_block.split("startplot:").at(-1);
-        console.log("RawPlotter", "Last plot data:", last_plot_text);
+            var plot_labels = last_plot_text.split("\n").at(0).trim().split(" ");
+            console.log("Plot labels:", plot_labels);
 
-        var plot_labels = last_plot_text.split("\n").at(0).trim().split(" ");
-        console.log("Plot labels:", plot_labels);
+            var plot_data_lines = text_to_data(last_plot_text);
 
-        var plot_data_lines = text_to_data(last_plot_text);
-
-        if (plot_labels.length === 0 || plot_labels[0] === "") {
-            showlegend = false;
-        }
-        console.log("Plot labels:", plot_labels);
-
-        if (config.plot.truncate) {
-            plot_data_lines = plot_data_lines.slice(-config.plot.history_len);
-        }
-        var plot_data = transpose(plot_data_lines);
-
-        if (config.plot.x_axis & (plot_labels.length > 1)) {
-            xLabel = plot_labels[0];
-            for (var i = 1; i < plot_data.length; i++) {
-                var curve = {};
-                curve["x"] = plot_data[0];
-                curve["y"] = plot_data[i];
-                curve["name"] = plot_labels[i];
-                curve["type"] = "scatter";
-                data.push(curve);
+            if (plot_labels.length === 0 || plot_labels[0] === "") {
+                showlegend = false;
             }
-        } else {
-            for (var i = 0; i < plot_data.length; i++) {
-                var curve = {};
-                curve["x"] = i;
-                curve["y"] = plot_data[i];
-                curve["name"] = plot_labels[i];
-                curve["type"] = "scatter";
-                data.push(curve);
-            }
-        }
-        var layout = {
-            showlegend: showlegend,
-            xaxis: {
-                title: xLabel,
-            },
-            height: height - 50,
-            width: width - 10,
-            padding: "0px",
+            console.log("Plot labels:", plot_labels);
 
-            margin: {
-                l: 30, // left
-                r: 10, // right
-                t: 20, // top
-                b: 20, // bottom
-            },
-        };
-
-        if (config.plot.enable_axis_limits) {
-            layout.yaxis = { range: [config.plot.y_min, config.plot.y_max] };
-            if (config.plot.x_axis) {
-                layout.xaxis.range = [config.plot.x_min, config.plot.x_max];
+            if (config.plot.truncate) {
+                plot_data_lines = plot_data_lines.slice(-config.plot.history_len);
             }
+            var plot_data = transpose(plot_data_lines);
+
+            if (config.plot.x_axis & (plot_labels.length > 1)) {
+                xLabel = plot_labels[0];
+                for (var i = 1; i < plot_data.length; i++) {
+                    var curve = {};
+                    curve["x"] = plot_data[0];
+                    curve["y"] = plot_data[i];
+                    curve["name"] = plot_labels[i];
+                    curve["type"] = "scatter";
+                    data.push(curve);
+                }
+            } else {
+                for (var i = 0; i < plot_data.length; i++) {
+                    var curve = {};
+                    curve["x"] = i;
+                    curve["y"] = plot_data[i];
+                    curve["name"] = plot_labels[i];
+                    curve["type"] = "scatter";
+                    data.push(curve);
+                }
+            }
+            var layout = {
+                showlegend: showlegend,
+                xaxis: {
+                    title: xLabel,
+                },
+                height: height - 50,
+                width: width - 10,
+                padding: "0px",
+
+                margin: {
+                    l: 30, // left
+                    r: 10, // right
+                    t: 20, // top
+                    b: 20, // bottom
+                },
+            };
+
+            if (config.plot.enable_axis_limits) {
+                layout.yaxis = { range: [config.plot.y_min, config.plot.y_max] };
+                if (config.plot.x_axis) {
+                    layout.xaxis.range = [config.plot.x_min, config.plot.x_max];
+                }
+            }
+        } catch (e) {
+            console.error("Exception thrown", e.stack);
         }
-    } catch (e) {
-        console.error("Exception thrown", e.stack);
     }
 
     const menuStructure = [

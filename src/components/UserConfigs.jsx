b473bf88b73e68a2fb2758fd50b24c8c77b01dd4
River Wang <urfdvw@gmail.com>
2025-07-12 11:27:21 -0400

    reset settings

--- src/components/UserConfigs.jsx
+++ src/components/UserConfigs.jsx
@@ -2,16 +2,30 @@ import { useContext } from "react";
 import { ConfigForms } from "../utilComponents/react-user-config";
 import AppContext from "../AppContext";
 import schemas from "../configs";
+import TabTemplate from "../utilComponents/TabTemplate";
 
 export default function UserConfigs() {
     const { appConfig, configTabSelection } = useContext(AppContext);
+    const menuStructure = [
+        {
+            text: "Reset to default",
+            handler: () => {
+                if (confirm("Are you sure you want to reset all settings and reload the page?")) {
+                    localStorage.clear();
+                    location.reload();
+                }
+            },
+        },
+    ];
     return (
-        <ConfigForms
-            schemas={schemas}
-            config={appConfig.config}
-            setConfig={appConfig.setConfig}
-            tabValue={configTabSelection.tabValue}
-            setTabValue={configTabSelection.setTabValue}
-        />
+        <TabTemplate title="Settings" menuStructure={menuStructure}>
+            <ConfigForms
+                schemas={schemas}
+                config={appConfig.config}
+                setConfig={appConfig.setConfig}
+                tabValue={configTabSelection.tabValue}
+                setTabValue={configTabSelection.setTabValue}
+            />
+        </TabTemplate>
     );
 }

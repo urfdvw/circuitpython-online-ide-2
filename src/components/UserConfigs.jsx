3dcb8f5e6397965b0e369ef0e610bdaf8d344dc2
River Wang <urfdvw@gmail.com>
2025-09-10 21:36:43 -0400

    clear idb in settings

--- src/components/UserConfigs.jsx
+++ src/components/UserConfigs.jsx
@@ -8,11 +8,19 @@ export default function UserConfigs() {
     const { appConfig, configTabSelection } = useContext(AppContext);
     const menuStructure = [
         {
-            text: "Reset to default",
-            handler: () => {
-                if (confirm("Are you sure you want to reset all settings and reload the page?")) {
+            text: "Reset IDE",
+            handler: async () => {
+                if (
+                    confirm(
+                        "Are you sure you want to reset all settings, remove all downloaded data and reload the page?"
+                    )
+                ) {
                     localStorage.clear();
                     location.reload();
+                    const dbs = await window.indexedDB.databases();
+                    dbs.forEach((db) => {
+                        window.indexedDB.deleteDatabase(db.name);
+                    });
                 }
             },
         },
